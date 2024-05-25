8c6a03ca9289a99a6f5d77c3cbc6404506b14a1a
alfathdrya <158708346+alfathdrya@users.noreply.github.com>
2024-05-25 22:42:46 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,11 @@
+<div class="card">
+            <div class="card-body">
+                <p>
+                <img src="gambar/webpro.png.jpeg" class="rounded float-end" alt="WebPro" height="200px">
+
+                    <b>Web progamming</b> Di sini kita dapat belajar membuat web
+                    <br>
+                    <b>Pengembangan web adalah istilah yang luas untuk pekerjaan yang terlibat dalam mengembangkan suatu situs web untuk internet atau intranet.</b>
+                </p>
+            </div>
+        </div>
